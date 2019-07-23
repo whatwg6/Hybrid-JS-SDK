99561b60564c49f8cc8ee82416e98738de230a96
77xi <4661042@qq.com>
2019-07-24 03:02:01 +0800

    refactor: web call Native support promise

--- src/NativeInterface.js
+++ src/NativeInterface.js
@@ -1,40 +1,19 @@
 const uuid = require('uuid/v4')
 
 class NativeInterface {
-  constructor ({ listeners, callBacks }) {
-    this.listeners = listeners
-    this.callBacks = callBacks
+  constructor ({ messagesObserver }) {
+    this.messagesObserver = messagesObserver
   }
 
-  dispatch (action, params) {
-    return new Promise((resolve, reject) => {
-      const id = uuid()
-      const actionListeners = this.listeners[action]
-
-      if (actionListeners && actionListeners.length) {
-        actionListeners.forEach(listener => {
-          listener(params)
-          resolve({
-            id,
-            status: `web receive ${action} success`
-          })
-        })
-      } else {
-        reject({
-          id,
-          status: `web unregister ${action}`
-        })
-      }
-    })
-  }
+  dispatch (action, params) {}
 
   callBack (id, params) {
-    return new Promise(resolve => {
-      this.callBacks[id](params)
-      resolve({
-        status: 'web receive callBack success'
-      })
-    })
+    this.messagesObserver[id] = {
+      id,
+      payload: {
+        params
+      }
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,34 +4,33 @@ const Bridge = require('./Bridge')
 const adaper = new Adaper()
 const bridge = new Bridge(adaper)
 
-// web -> native -> web
-
-// web subscribe base/themeChange
-const unsubscribe = bridge.listen('base/themeChange', ({ theme }) =>
-  console.log(`theme is ${theme}`)
-)
-
-// native publish base/themeChange
-webApp
-  .dispatch('base/themeChange', { theme: 'light' })
-  .then(success => console.log(success))
-  .catch(err => console.log(err))
-
-// unsubscribe base/themeChange
-unsubscribe()
 
 // web -> native
 
 // web dispatch base/openURL
 bridge
-  .dispatch('base/openURL', { url: 'url' }, function(params) {
-    // 不希望传递 callBack。希望可以处理成 Promise
-    console.log(`openURL ${params}`)
-  })
-  // .then(success => console.log(`openURL ${success}`))
-  // .catch(err => console.log(`openURL ${err}`))
+  .dispatch('base/openURL', { url: 'url' })
+  .then(({ status }) => console.log(`openURL`, status))
+  .catch(err => console.log(`openURL ${err}`))
 
 // native callBack base/openURL
 setTimeout(() => {
-  webApp.callBack('1', { status: 'success/fail' }).then(arg => console.log(arg))
+  webApp.callBack('1', { status: 'success' })
+  webApp.callBack('2', { status: 'fail' })
 })
+
+// web -> native -> web
+
+// web subscribe base/themeChange
+// const unsubscribe = bridge.listen('base/themeChange', ({ theme }) =>
+//   console.log(`theme is ${theme}`)
+// )
+
+// native publish base/themeChange
+// webApp
+//   .dispatch('base/themeChange', { theme: 'light' })
+//   .then(success => console.log(success))
+//   .catch(err => console.log(err))
+
+// unsubscribe base/themeChange
+// unsubscribe()

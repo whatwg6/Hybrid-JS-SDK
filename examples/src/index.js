c114fd9b53671b8763908beab1a1c017f630bf45
0x0006e <yaoxc07@gmail.com>
2020-03-04 11:47:47 +0800

    Update: examples

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,24 +1,29 @@
-// native inject js into webview
 import simulator from "../../build/simulator";
+import { log } from "./util";
 
+log("native inject js into webview");
 simulator();
 
-// webviwe import hybrid js sdk
+log("webviwe import hybrid js sdk");
 import hybrid from "../../build";
 
-// web -> native
+log(
+  'web -> native: hybrid.dispatch("answer/writeAnswer", { id: 123456789 })'
+);
 hybrid
   .dispatch("answer/writeAnswer", { id: 123456789 })
   .then(console.log)
   .catch(console.error);
 
+log('hybrid.dispatch("base/openURL", { url: "www.twitter.com" });');
 hybrid
   .dispatch("base/openURL", { url: "www.twitter.com" })
   .then(console.log)
   .catch(console.error);
 
-// web -> native -> web
+log(
+  'web -> native -> web: hybrid.listen("base/networkChange", console.log)'
+);
 const unsubscribe1 = hybrid.listen("base/networkChange", console.log);
 const unsubscribe2 = hybrid.listen("base/networkChange", console.log);
-
 unsubscribe2();
---
+++ examples/src/util.js
@@ -0,0 +1,5 @@
+function log(content) {
+  console.log(`%c ${content}`, `font-size:15px;`);
+}
+
+export { log };

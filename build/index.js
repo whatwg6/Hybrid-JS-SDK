965892f446b15066e51363d558ddbc8b07293d5f
0x0006e <yaoxc07@gmail.com>
2020-03-13 17:09:27 +0800

    build: EventEmit & examples

--- build/index.js
+++ build/index.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Adaper_1 = __importDefault(require("./core/Adaper"));
 const Bridge_1 = __importDefault(require("./core/Bridge"));
-const EventEmit_1 = __importDefault(require("./core/EventEmit"));
+const EventEmitter_1 = __importDefault(require("./core/EventEmitter"));
 const simulator_1 = __importDefault(require("./simulator"));
 exports.simulator = simulator_1.default;
-const eventEmit = new EventEmit_1.default();
+const eventEmit = new EventEmitter_1.default();
 const adaper = new Adaper_1.default(eventEmit);
 const bridge = new Bridge_1.default(adaper);
 const dispatch = (action, args) => bridge.dispatch(action, args);

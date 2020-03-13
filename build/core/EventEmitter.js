34bd1bb4eb0ab73c000e1e1b040e1ef3f13984f5
0x0006e <yaoxc07@gmail.com>
2020-03-13 17:42:58 +0800

    build: EventEmitter

--- build/core/EventEmitter.js
+++ build/core/EventEmitter.js
@@ -13,21 +13,21 @@ class EventEmitter {
     }
     on(event, handler) {
         var _a;
-        __classPrivateFieldGet(this, _listener).set(event, [
-            ...(_a = __classPrivateFieldGet(this, _listener).get(event)) !== null && _a !== void 0 ? _a : [],
-            handler
-        ]);
+        if (!__classPrivateFieldGet(this, _listener).has(event)) {
+            __classPrivateFieldGet(this, _listener).set(event, new Set());
+        }
+        (_a = __classPrivateFieldGet(this, _listener).get(event)) === null || _a === void 0 ? void 0 : _a.add(handler);
     }
     emit(event, params) {
         var _a;
         (_a = __classPrivateFieldGet(this, _listener).get(event)) === null || _a === void 0 ? void 0 : _a.forEach(f => f(params));
     }
     remove(event, handler) {
-        var _a, _b, _c;
+        var _a, _b;
         if (__classPrivateFieldGet(this, _listener).has(event)) {
-            __classPrivateFieldGet(this, _listener).set(event, (_b = (_a = __classPrivateFieldGet(this, _listener).get(event)) === null || _a === void 0 ? void 0 : _a.filter(f => f !== handler)) !== null && _b !== void 0 ? _b : []);
+            (_a = __classPrivateFieldGet(this, _listener).get(event)) === null || _a === void 0 ? void 0 : _a.delete(handler);
         }
-        if (!((_c = __classPrivateFieldGet(this, _listener).get(event)) === null || _c === void 0 ? void 0 : _c.length)) {
+        if (!((_b = __classPrivateFieldGet(this, _listener).get(event)) === null || _b === void 0 ? void 0 : _b.size)) {
             __classPrivateFieldGet(this, _listener).delete(event);
         }
     }

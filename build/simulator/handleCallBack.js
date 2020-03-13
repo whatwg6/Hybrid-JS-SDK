7c3e3704f3156fff1ca949a50cbf8e21bd2eb9de
0x0006e <yaoxc07@gmail.com>
2020-03-13 20:55:50 +0800

    build: remove build folder

--- build/simulator/handleCallBack.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handleCallBack() {
-    setTimeout(() => global.webApp.dispatch("base/networkChange", {
-        currentNetwork: "4G"
-    }));
-    setTimeout(() => global.webApp.dispatch("base/networkChange", "3G"), 2 * 1000);
-}
-exports.default = handleCallBack;

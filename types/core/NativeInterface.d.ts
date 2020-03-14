caf0a4a14a657b638cd3edf9ca12580831ed4f6c
0x0006e <yaoxc07@gmail.com>
2020-03-14 17:55:01 +0800

    feat: NativeInterface typing

--- types/core/NativeInterface.d.ts
+++ types/core/NativeInterface.d.ts
@@ -3,13 +3,13 @@ import { StatusString } from "./Status";
 declare class NativeInterface {
     readonly eventEmitter: EventEmit;
     constructor(eventEmitter: EventEmit);
-    dispatch<T>(event: string, params?: T): void;
+    dispatch<T>(event: string, params?: T): Promise<string>;
     callBack<T>(callbackMessage: {
         readonly id: string;
         payload: {
             status: StatusString;
             params?: T;
         };
-    }): void;
+    }): Promise<string>;
 }
 export default NativeInterface;

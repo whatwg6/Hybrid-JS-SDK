8a1da54cd50c3cbc896735a1b039050d5352b21b
0x0006e <yaoxc07@gmail.com>
2020-03-14 10:56:30 +0800

    build: typing

--- types/core/EventEmitter.d.ts
+++ types/core/EventEmitter.d.ts
@@ -2,6 +2,6 @@ declare class EventEmitter {
     #private;
     on(event: string, handler: Function): void;
     emit<T>(event: string, params?: T): void;
-    remove(event: string, handler: Function): void;
+    remove(event: string, handler?: Function): void;
 }
 export default EventEmitter;
